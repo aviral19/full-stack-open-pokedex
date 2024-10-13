dd8b24f60e4bfaee655f49ff667c8d889bf3e115
Aviral Bajpai <aviralbajpai@Avirals-MacBook-Pro.local>
2024-10-13 18:44:58 +0530

    Eslint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,23 +27,5 @@ module.exports = {
       "error",
       "unix"
     ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
   }
 }
